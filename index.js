0a2b7c222415ca54f912cd5b2f03b2ef3608ec82
abigailmgarcia <abigailmg31@gmail.com>
2023-07-27 21:59:54 -0500

    debugged code errors, shapeColor and textColor

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateShapes = require("./lib/shapes");
+const  generateSvg = require('./lib/shapes')
+
+const colorValidator = (value) => {
+    const isValid = /^([a-z]+|\#[0-9A-Fa-f]{6}|\#[0-9A-Fa-f]{3})$/i.test(value);
+    return isValid ? true : 'Please enter a valid color name or hexadecimal code';
+}
 
 inquirer
     .prompt([
@@ -8,11 +13,15 @@ inquirer
             type:'input',
             name: 'text',
             message: 'Enter 3 characters for your logo:',
+            validate: function(value){
+                return value.length <= 3;
+            }
         },
         {
             type: 'input',
-            name: 'text color',
+            name: 'textColor',
             message:'Please enter your text color:',
+            validate: colorValidator,
         },
         {
             type: "list",
@@ -22,18 +31,15 @@ inquirer
         },
         {
             type: 'input',
-            name: 'shape color',
+            name: 'shapeColor',
             message: 'please enter your shape color',
+            validate: colorValidator,
         },
     ])
     .then((answers) => {
-        const svgContent = generateShapes(answers);
+        const svgContent = generateSvg(answers);
 
-        fs.writeFile('logo.svg', svgContent, err =>{
-            if(err){
-                console.log(err);
-                return
-            }
-            console.log('logo saved as a svg');
+        fs.writeFile('logo.svg', svgContent, (err) =>{
+         err ? console.error(err) : console.log('Generated logo.svg')
         });
     });
